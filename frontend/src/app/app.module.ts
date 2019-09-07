07554bd736fec9019c071d3075077042a824e570
Youngermaster <jmyoungh@eafit.edu.co>
2019-09-07 09:52:10 -0500

    I added the core

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { from } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
